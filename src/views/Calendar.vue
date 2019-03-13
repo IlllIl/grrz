2874441d3d83a4a657a2637ae6d877b0e3cf5913
Sebastian Köpsel <s.koepsel@igniti.de>
2019-03-13 12:38:48 +0100

    calendar basics

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,35 +1,62 @@
 <template>
     <div class="cal-wrap">
         <full-calendar :config="config" :events="events"></full-calendar>
+
+                <b-modal ref="myModalRef" hide-footer v-bind:title="student.name + 's tasks'">
+
+                    <div v-if="student" >
+
+                        <TodoList v-bind:student="student"></TodoList>
+                    </div>
+                    <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
+                </b-modal>
+
     </div>
 </template>
 
 <script lang="ts">
+    import TodoList from "../components/TodoList";
+
     export default {
         name: "Calendar",
+        components: {TodoList},
+        methods: {
+            showModal() {
+                this.$refs.myModalRef.show()
+            },
+
+            hideModal() {
+                this.$refs.myModalRef.hide()
+            }
+
+        },
         data() {
             let events = this.$store.getters.getEvents;
 
+            let that = this;
             return {
-                events: events.map(val=>{
-                    let x =  {
+                events: events.map(val => {
+                    return {
                         start: val.start,
-                        end:val.end,
-                        dow:[val.dow],
-                        title:val.title
-                    }
-                    return x;
+                        end: val.end,
+                        dow: [val.dow],
+                        title: val.title,
+                        id: val.student
+                    };
                 }),
                 config: {
                     height: 500,
 
                     defaultView: 'listWeek',
-                    eventRender: function (event, element) {
-                        console.log(element)
-                        element.click(()=>console.log("click"))
+                    eventRender:  (event, element)=> {
+                        element.click(() => {
+                            this.student = that.$store.getters.getStudentById(event.id);
+                            that.showModal()
+                        })
                     },
 
                 },
+                student: {}
 
             }
         }
@@ -42,7 +69,6 @@
         height: 400px;
 
     }
-
 
 
 </style>
